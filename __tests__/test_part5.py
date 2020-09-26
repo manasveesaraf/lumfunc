7aa1483c9016092c05f0de0d8eb1b9d443c88e80
Manasvee Saraf <manasvee.saraf.16@ucl.ac.uk>
2020-09-26 16:06:10 +0530

    testing pytest.approx with tolerances

--- __tests__/test_part5.py
+++ __tests__/test_part5.py
@@ -738,15 +738,15 @@ def test_get_double_schechter_phi_rudimentarily_no_plot( ):
         0.007510562710100609]
     phi_test = [ round(x,2) for x in phi_test ]
     assert phi_result == phi_test
-    assert round(chi_sq_sch2_result,2) == round(0.8888283543610924,2)
-    assert round(M_star_result,2) == round(-22.303908380116704,2)
-    assert round(M_star_err_result,2) == round(0.26464127945271887,2)
-    assert round(phi_star_1_result,2) == round(0.0009668887609189701,2)
-    assert round(phi_star_err_1_result,2) == round(0.000640187578339006,2)
-    assert round(phi_star_2_result,2) == round(-1.0900241221219484,2)
-    assert round(phi_star_err_2_result,2) == round(0.7987986322969173,2)
-    assert round(alpha_star_1_result,2) == round(0.0001418318772494868,2)
-    assert round(alpha_star_err_1_result,2) == round(0.0008399596540331241,2)
-    assert round(alpha_star_2_result,2) == round(-1.774506451062984,2)
+    assert chi_sq_sch2_result == approx(0.8888283543610924, rel=1e-6, abs=9e-4)
+    assert M_star_result == approx(-22.303908380116704, rel=1e-6, abs=9e-4)
+    assert M_star_err_result == approx(0.26464127945271887, rel=1e-6, abs=9e-4)
+    assert phi_star_1_result == approx(0.0009668887609189701, rel=1e-6, abs=9e-4)
+    assert phi_star_err_1_result == approx(0.000640187578339006, rel=1e-6, abs=9e-4)
+    assert phi_star_2_result == approx(-1.0900241221219484, rel=1e-6, abs=9e-4)
+    assert phi_star_err_2_result == approx(0.7987986322969173, rel=1e-6, abs=9e-4)
+    assert alpha_star_1_result == approx(0.0001418318772494868, rel=1e-6, abs=9e-4)
+    assert alpha_star_err_1_result == approx(0.0008399596540331241, rel=1e-6, abs=9e-4)
+    assert alpha_star_2_result == approx(-1.774506451062984, rel=1e-6, abs=9e-4)
     assert alpha_star_err_2_result == approx(0.9946532141625982, rel=1e-6, abs=9e-4)
 # -----------------------
